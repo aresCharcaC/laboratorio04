131632604b2208dfab42e2fe2a0be93fac4aec5c
aresCharcaC <henry.charca@tecsup.edu.pe>
2024-09-19 17:05:00 -0500

    se añade un scaffold

--- app/src/main/java/com/example/laboratorio04/app.kt
+++ app/src/main/java/com/example/laboratorio04/app.kt
@@ -7,9 +7,28 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun app() {
-    
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Scaffold") }
+            )
+        }
+    ) { innerPadding ->
+        // Contenido principal
+        Text(
+            text = "Probando",
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun appPreview(){
+    app()
 }
